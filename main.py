8d019ce0a66dd3e335dd32f5f6c30e35aa5f0fa4
szche <github@chadam.pl>
2020-06-05 19:08:52 +0200

    Added history tracking

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ url = "https://en.wikipedia.org/w/api.php?"
 pages = []
 queue = []
 flagged = []
+msgs = []
 
 #Find all the external wiki links inside the specified wiki article
 def getLinks(pageName):
     links = []
-    print("Page: {}".format(pageName))
-    response = requests.get(url+"action=parse&prop=links&format=json&page={}".format(pageName))
+    print("Page: {}".format(pageName[-1]))
+    response = requests.get(url+"action=parse&prop=links&format=json&page={}".format(pageName[-1]))
     #If the page doesn't exist return empty array
     try:
         data = response.json()["parse"]["links"]
@@ -18,9 +19,15 @@ def getLinks(pageName):
         return []
     for item in data:
         if item["ns"] != 0: continue
-        if item['*'] not in links: links.append(item['*'])
+        if item['*'] not in links: 
+            temp = [item for item in pageName]
+            temp.append(item['*'])
+            links.append(temp)
     return links
 
+def getCurrentLinks(linkHistory):
+    links = [item[-1].lower() for item in linkHistory]
+    return links
 
 while True:
     inputPage = input("Input the name of the page: ")
@@ -35,29 +42,39 @@ while len(pages) != 1:
     print("Pages: {}".format(pages))
     page1 = pages[0]
     page2 = pages[1]
-    queue.extend(getLinks(page1))
+    queue.extend(getLinks([page1]))
     for subPage in queue:
+        currentPage = subPage[-1]
         #We don't want to visit the same page more than once for one pair
-        if subPage in flagged:
+        if currentPage in flagged:
             queue.remove(subPage)
             continue
         subPageLinks = getLinks(subPage)
-        if page2 in subPageLinks:
+        if page2.lower() in getCurrentLinks(subPageLinks):
             print("-"*20)
-            print("{} found in {}".format(pages, subPage))
-            print("https://en.wikipedia.org/wiki/{}".format(subPage))
+            alert = "{} found in:  {}".format(pages[1], " -> ".join(subPage))
+            print(alert)
+            msgs.append(alert)
+            print("https://en.wikipedia.org/wiki/{}".format(subPage[-1]))
             pages.remove(page1)
             pages.remove(page2)
-            pages.append(subPage)
+            pages.append(subPage[-1])
             print("-"*20)
             break
-        flagged.append(subPage)
+        flagged.append(subPage[-1])
         queue.remove(subPage)
         queue.extend(subPageLinks)
     #Once we found a match for the pair, clear the queue and flagged pages
     flagged.clear()
     queue.clear()
 
+print("-"*50)
+print("\t\tRESULTS")
+print("-"*50)
+for message in msgs:
+    print(message)
+
+print("-" * 20)
 print("The best match is: {}".format(pages[0]))
 print("https://en.wikipedia.org/wiki/{}".format(pages[0]))
 
